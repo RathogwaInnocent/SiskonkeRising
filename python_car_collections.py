c2e6e4a39fcc3759824dfaa5eaf94ab20a3223a2
RathogwaInnocent <RathogwaInnocent@users.noreply.github.com>
2016-07-15 02:53:23 +0200

    Add files via upload
    car collections update

--- python_car_collections.py
+++ python_car_collections.py
@@ -1,5 +1,19 @@
-myFavouriteCars = ['BMW_M4', 'VW_GTI_7', 'FORD_MUSTENG_GT']
+import sys
+myFavouriteCars = {1:'BMW_M4', 2:'VW_GTI_7',3: 'FORD_MUSTENG_GT'}
 
 print(myFavouriteCars)
 for car in myFavouriteCars:
-    print  car
+    print  (car,myFavouriteCars[car])
+
+# OR
+
+for i, v in enumerate(['BMW_M4','VW_GTI_7','FORD_MUSTENG_GT']):
+    print i, v
+
+# OR
+
+myCarCollection = ['BMW_M4','VW_GTI_7','FORD_MUSTENG_GT']
+for f in sorted(set(myCarCollection)):
+    print f
+
+
